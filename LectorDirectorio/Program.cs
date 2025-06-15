05a038409ebe428a3325fd866701aa128882ce0d
Lautaro G Cruz Srur <105937766+LautaroCruzSrur@users.noreply.github.com>
2025-06-14 22:05:16 -0300

    terminando el archivo

--- LectorDirectorio/Program.cs
+++ LectorDirectorio/Program.cs
@@ -7,13 +7,21 @@ string ruta = Console.ReadLine() ?? ""; //VOlver a solicitar si es vacio
 
 if (!File.Exists(ruta)) // Una vez que sabemos que existe listar las carpetas y archivos(KB)
 {
-    DirectoryInfo  directory = new DirectoryInfo (ruta);
+    DirectoryInfo directory = new DirectoryInfo(ruta);
     FileInfo[] files = directory.GetFiles();
+    List<string> Lineas = new List<string>();
 
     foreach (FileInfo file in files)
-        {
-            Console.WriteLine($"Nombre: {file.Name}, Tamaño: {file.Length} bytes, Modificado: {file.LastWriteTime}");
-        }
+    {
+        Console.WriteLine($"Nombre: {file.Name}, Tamaño: {file.Length} bytes");
+        Lineas.Add($"Nombre: {file.Name}");
+        Lineas.Add($"Tamaño: {file.Length}");
+        Lineas.Add($"Ultimo acceso: {file.LastAccessTime}");
+
+    }
+    string rutaParaGuardar = Path.Combine(ruta, "listado.csv");
+    File.WriteAllLines(rutaParaGuardar, Lineas);
+        
 }
 
 
