01f83ade6a1617d9f5fe76cb927e54a59cd2ca57
Lautaro G Cruz Srur <105937766+LautaroCruzSrur@users.noreply.github.com>
2025-06-14 21:50:08 -0300

    haciendo el punto 1

---
+++ LectorDirectorio/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+
+Console.WriteLine("Ingrese el 'path' de un directorio para analizar ");
+string ruta = Console.ReadLine() ?? ""; //VOlver a solicitar si es vacio
+
+if (!File.Exists(ruta)) // Una vez que sabemos que existe listar las carpetas y archivos(KB)
+{
+    DirectoryInfo  directory = new DirectoryInfo (ruta);
+    FileInfo[] files = directory.GetFiles();
+
+    foreach (FileInfo file in files)
+        {
+            Console.WriteLine($"Nombre: {file.Name}, Tamaño: {file.Length} bytes, Modificado: {file.LastWriteTime}");
+        }
+}
+
+
+
+
